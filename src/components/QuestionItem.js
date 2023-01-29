a11b6f36eed161563cf4edb282aee43b3d662fea
Abdullah-Naifeh <abdna59@gmail.com>
2023-01-29 17:49:16 +0300

    Completed Assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question,onItemDelete,onPatchQuestion}) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,37 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete(){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => onItemDelete(question));
+  }
+
+  // function handleAnswerChange() {
+  //   fetch(`http://localhost:4000/items/${id}`, {
+  //     method: "PATCH",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       "correctIndex": correctIndex
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((updatedItem) => console.log(updatedItem));
+  // }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={(e) => onPatchQuestion(id, { correctIndex: e.target.value })}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+  const [Questions,setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then((response) => response.json())
+    .then((questions) => setQuestions(questions))
+  },[])
+
+  function onItemDelete(deletedItem) {
+    const updatedItems = Questions.filter((item) => item.id !== deletedItem.id);
+    setQuestions(updatedItems);
+  }
+
+  const patchQuestion = async (id, updatedQuestion) => {
+    const response = await fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedQuestion),
+    });
+    const updatedQuestionFromServer = await response.json();
+    const updatedQuestions = Questions.map((question) =>
+      question.id === id ? updatedQuestionFromServer : question
+    );
+    setQuestions(updatedQuestions);
+  };
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{Questions.map(question => <QuestionItem key={question.id} question={question} onItemDelete={onItemDelete} onPatchQuestion={patchQuestion}/>)}</ul>
     </section>
   );
 }
